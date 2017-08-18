2da762bd06ce7b2216ef654492f9e8acbcae5768
Arthur van der Peijl <aavanderpeijl@gmail.com>
2017-08-18 13:29:58 +0200

    Updated based on script of io-core-unifi

--- copy/var/zoneinit/includes/10-delegated-dataset.sh
+++ copy/var/zoneinit/includes/10-delegated-dataset.sh
@@ -2,13 +2,18 @@
 UUID=$(mdata-get sdc:uuid)
 DDS=zones/$UUID/data
 
-if ! zfs list $DDS > /dev/null; then
-        # No delegated dataset configured
-        exit 0
+if zfs list ${DDS} 1>/dev/null 2>&1; then
+        zfs create ${DDS}/unifi-data || true
+        zfs create ${DDS}/unifi-logs || true
+        zfs create ${DDS}/unifi-work || true
+
+        zfs set mountpoint=/opt/local/UniFi/data ${DDS}/unifi-data
+        zfs set mountpoint=/opt/local/UniFi/logs ${DDS}/unifi-logs
+        zfs set mountpoint=/opt/local/UniFi/work ${DDS}/unifi-work
+
+        zfs set compression=lz4 ${DDS}
 fi
 
-zfs set mountpoint=/srv $DDS
-zfs set compression=lz4 $DDS
-
-
-
+chown unifi:other /opt/local/UniFi/data || true
+chown unifi:other /opt/local/UniFi/logs || true
+chown unifi:other /opt/local/UniFi/work || true
